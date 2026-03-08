c366b1aee37570dab01b84ab5e1163935dbae5f8
Pedro Henrique <ph171695@gmail.com>
2026-03-08 12:03:27 -0300

    Primeira atividade POO

---
+++ Aula01/src/main/java/com/mycompany/aula/Exercicio01.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.aula;
+import java.util.Scanner;
+
+public class Exercicio01 {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);/*Define o objeto da criação*/
+        System.out.println("Digite os quatro numeros inteiros da operacao:");/*Declara os números pedidos*/
+        
+            int num1 = scanner.nextInt();/*Digita o primeiro número*/
+            int num2 = scanner.nextInt();/*Digita o segundo número*/
+            int num3 = scanner.nextInt();/*Digita o terceiro número*/
+            int num4 = scanner.nextInt();/*Digita o quarto número*/
+            
+            int soma = num1 + num2 + num3 + num4;/*Código da soma dos números*/
+        System.out.println("A soma dos numeros e:" +soma); /*Impressão dos resultados*/
+        
+    }
+}
