e53363d6b9a9b4d99ed7623d7feff0c8ed38c6bf
Pedro Henrique <ph171695@gmail.com>
2026-03-08 13:53:11 -0300

    Segunda atividade POO

--- Aula01/src/main/java/com/mycompany/aula/Exercicio02.java
+++ Aula01/src/main/java/com/mycompany/aula/Exercicio02.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.aula;
-
+import java.util.Scanner;
 /**
  *
  * @author Pedro Henrique
@@ -11,6 +11,15 @@ package com.mycompany.aula;
 public class Exercicio02 {
     
     public static void main(String[] args) {
-        System.out.println("Teste");
+        Scanner scanner = new Scanner (System.in);/*Define o objeto da criação*/
+        System.out.println("Digite os tres numeros que serao colocados na media aritmetica:");/*Declara os números pedidos*/
+        
+            float nota1 = scanner.nextInt();/*Digita o primeiro número*/
+            float nota2 = scanner.nextInt();/*Digita o segundo número*/
+            float nota3 = scanner.nextInt();/*Digita o terceiro número*/
+            
+            float soma = (nota1 + nota2 + nota3) / 3;
+            
+        System.out.println("A media aritmetica e:" +soma);
     }
 }
